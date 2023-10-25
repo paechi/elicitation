ae31ef9dceab325aa2ac7d9af6a46cd97f40826d
Madina <flinchy_m@bk.ru>
2023-10-26 03:12:54 +0600

    First commit

--- eval_performance.py
+++ eval_performance.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     data = load_data(args.filename)
-    reg = np.sum([v[0] for k, v in data.items()])/len(data)
-    mcts = np.sum([v[1] for k, v in data.items()])/len(data)
+    reg = np.sum([v[1] for k, v in data.items()])/len(data)
+    mcts = np.sum([v[2] for k, v in data.items()])/len(data)
     print('Average mcts performance: %s vs average regular elicitation performance: %s' % (mcts, reg))
     print('MCTS is effective by', 1 - mcts/reg)
 
